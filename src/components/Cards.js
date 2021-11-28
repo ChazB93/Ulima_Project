31bd3fdaf1d81daf97ea050532bf069b5e951765
Charles <bizindavyiCharles2016@gmail.com>
2021-11-28 02:23:41 +0100

    Footer completed

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -21,6 +21,28 @@ function Cards() {
               label="IoT Solutions"
               path="/services" 
             />
+            
+          </ul>
+          <ul className="cards__items">
+            <CardItems
+              src="images/img-sup.jpg"
+              text="Call us and just relax. we provide quickly and reliable IT Support to individuals and companies"
+              label="IT Technical Support"
+              path="/services" 
+            />
+             <CardItems
+              src="images/img-big.jpg"
+              text="We have the organizations to deal with their data and provide to them insights that help to make good decisions for their businesses"
+              label="Big Data"
+              path="/services" 
+            />
+            <CardItems
+              src="images/img-sta.jpg"
+              text="We help your staff to meet the needed competences to grow your businesses by traiing them"
+              label="Trainings"
+              path="/services" 
+            />
+            
           </ul>
         </div>
       </div>
